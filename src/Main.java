6ff34ab241d63ea7c456bcd5e9c2b093cc71ff2f
xxrang <xxrrang2@gmail.com>
2025-03-08 00:57:05 +0900

    1주차 과제 수정

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -12,17 +7,35 @@ public class Main {
 
         String title = sc.nextLine();
         float score = sc.nextFloat();
+
+        String input1 = sc.nextLine();
+        String input2 = sc.nextLine();
+        String input3 = sc.nextLine();
+        String input4 = sc.nextLine();
+        String input5 = sc.nextLine();
+        String input6 = sc.nextLine();
+        String input7 = sc.nextLine();
+        String input8 = sc.nextLine();
+        String input9 = sc.nextLine();
+        String input10 = sc.nextLine();
+
+        System.out.println("[ " + title + " ]");
+
         int convertInt = (int)score;
         double percentScore = convertInt * 100 / 5.0;
 
-//        int i;
-
-        System.out.println("[ " + title + " ]");
         System.out.println("별점 : " + convertInt + " (" + percentScore + "%)");
 
-//        for (i=1; i<11; i++) {
-//            String input = sc.nextLine();
-//            System.out.println(i + ". " + input);
-//        }
+        System.out.println("1. " + input1);
+        System.out.println("2. " + input2);
+        System.out.println("3. " + input3);
+        System.out.println("4. " + input4);
+        System.out.println("5. " + input5);
+        System.out.println("6. " + input6);
+        System.out.println("7. " + input7);
+        System.out.println("8. " + input8);
+        System.out.println("9. " + input9);
+        System.out.println("10. " + input10);
+
     }
 }
